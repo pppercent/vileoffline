55d8b00c08f145089ab36c64860692b03ff46bd5
pppercent <tetetetetetfdfdsf@proton.me>
2025-08-12 11:20:58 +0200

    Delete agent/init.ts

--- agent/init.ts
+++
@@ -1,7 +0,0 @@
-import { addDebugFile } from "./debugmenu.js";
-import { installHooks } from "./mainHooks.js";
-import { applyPatches } from "./patches.js";
-
-installHooks();
-applyPatches();
-addDebugFile();
