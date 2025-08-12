e0fc7d884b27164a73449153354719c01048d1af
pppercent <tetetetetetfdfdsf@proton.me>
2025-08-12 11:21:27 +0200

    Delete agent/patches.ts

--- agent/patches.ts
+++
@@ -1,8 +0,0 @@
-import { base } from "./definitions.js";
-import { Offsets } from "./offsets.js";
-
-export function applyPatches() {
-    // cursed code; maybe i should've used js
-    (Memory as any).writeUtf8String(base.add(Offsets.EditControlsBrawler), "Silencer\0");
-    //(Memory as any).writeUtf8String(base.add(Offsets.EditControlsMap), "Tutorial\0"); crashes
-}
